9f68c795717bea5ef85ad68b53174716a96a3240
Edu Guerreiro <105218997+Edu-Argolo-Guerreiro@users.noreply.github.com>
2023-08-17 12:09:48 -0300

    Add files via upload

---
+++ aula10b/index.js
@@ -0,0 +1,9 @@
+let umaString = "Um texto";
+
+console.log(umaString.charAt(7));
+console.log(umaString.replace('Um', 'Enorme'));
+console.log(umaString.length);
+console.log(umaString.slice(4, 7));
+console.log(umaString.split('t'));
+console.log(umaString.toUpperCase());
+
