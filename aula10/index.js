fc5dfdca6a660f2eed6a9cada0101b4f92038d01
Edu Guerreiro <105218997+Edu-Argolo-Guerreiro@users.noreply.github.com>
2023-08-17 12:07:01 -0300

    Delete aula10 directory

--- aula10/index.js
+++
@@ -1,9 +0,0 @@
-let umaString = "Um texto";
-
-console.log(umaString.charAt(7));
-console.log(umaString.replace('Um', 'Enorme'));
-console.log(umaString.length);
-console.log(umaString.slice(4, 7));
-console.log(umaString.split('t'));
-console.log(umaString.toUpperCase());
-
