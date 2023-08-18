bb5b5af6adcb9629182fa7d87bc2e7db5b375d47
Edu Guerreiro <105218997+Edu-Argolo-Guerreiro@users.noreply.github.com>
2023-08-18 09:44:07 -0300

    Add files via upload

---
+++ b_aula11/Numbers.js
@@ -0,0 +1,10 @@
+let num1 = 15; //Number
+let num2 = 2.5; //Number
+
+//console.log (num1.toString() + num2); -> Transformei o número em String e depois fiz a junção de valores.
+//num1 = num1.toString(); -> Transformei o número em String.
+//num1.toString(2); -> Vejo a representação binária do número.
+//num1.toFixed(2); -> Isso diz quantas casas decímais possuirá.
+//Number.isInteger(num1); -> Informa se é True or False, caso o número seja inteiro ou flutuante.
+
+//num1 = Number(num1.toFixed(2));
